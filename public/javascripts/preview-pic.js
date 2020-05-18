9f22b6b6a55f31dfd672eb70e61f559b1bdd5020
Warinthorn <>
2020-05-18 15:34:42 +0700

    dCommit all file

---
+++ public/javascripts/preview-pic.js
@@ -0,0 +1,75 @@
+/*  ==========================================
+    SHOW UPLOADED IMAGE
+* ========================================== */
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#imageResult')
+        .attr('src', e.target.result);
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$(function () {
+  $('#upload').on('change', function () {
+    readURL(input);
+  });
+});
+
+/*  ==========================================
+    SHOW UPLOADED IMAGE NAME
+* ========================================== */
+var input = document.getElementById('upload');
+var infoArea = document.getElementById('upload-label');
+
+input.addEventListener('change', showFileName);
+
+function extraction(text) {
+  var str = text.split("\n");
+  console.log(str);
+  var i;
+
+  for (i = 0; i < str.length; i++) {
+    console.log(i);
+    if (i == 1) {
+      console.log(i);
+    }
+  }
+}
+
+function showFileName(event) {
+
+  var input = event.srcElement;
+  var file = input.files[0].name;
+
+  infoArea.textContent = 'File name: ' + file;
+
+  const formData = new FormData() // ข้อมูลเป็นไฟล์ เอาใส่ FormData แบบนี้ ปล.ไม่ต้องเป็น file ก็ส่งแบบนี้ได้
+  formData.append('image', input.files[0]) // เอาไฟล์ยัดใส่ FormData ใช้ชื่อ Field ว่า "image"
+
+
+  // ปกติถ้าไม่ได้ส่ง file จะส่งเป็น json แบบนี้
+  // const formData = { fileName: "Wa", message: "wawawa" } 
+
+  $.ajax({
+    url: '/test',
+    type: 'post',
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function (data) {
+      console.log('success');
+      // console.log("This file " + file);
+      alert(data.message);
+    },
+    error: function (err) {
+      console.log('error: ', err);
+    }
+  });
+}
+
+
---
+++ routes/index.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+const extract = require("../utility/extrac-text.js");
+const { createWorker } = require('tesseract.js');
+
+
+var diskstorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images"); // here we specify the destination . in this case i specified the current directory
+  },
+  filename: function (req, file, cb) {
+    // console.log("This console log" + file);
+    cb(null, file.originalname);// here we specify the file saving name . in this case i specified the original file name
+  },
+});
+
+var upload = multer({
+  storage: diskstorage,
+  /*File Filter*/
+  fileFilter: (req, file, cb) => { // มีบัคที่ไฟล์ tiff ไม่แสดงหน้าเว็ป
+    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg" || file.mimetype == "image/tiff") {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error('Only .png, .jpg and .jpeg .tif format allowed!'));
+    }
+  }
+});
+function ValidateEmail(text) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (text.match(mailformat)) {
+    console.log(text);
+    return true;
+  }
+  else {
+    console.log("You have entered an invalid email address!");
+    return false;
+  }
+};
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index');
+
+});
+
+router.post('/test', upload.single("image"), function (req, res, next) {
+  // console.log(req.file);
+  var image = "./public/images/" + req.file.originalname;
+  console.log(image);
+  const worker = createWorker({
+    logger: m => console.log(m), // Add logger here
+  });
+
+  (async () => {
+    await worker.load();
+    await worker.loadLanguage('eng+tha');
+    await worker.initialize('eng+tha');
+    const { data: { text } } = await worker.recognize(image);
+    console.log(text);
+    var str = text.split("\n");
+    console.log(text.split("\n"));
+    // console.log(typeof str);
+    // console.log(str.length);
+    // console.log(extract.ext(text));
+    let valid = new extract.ext(text);
+    valid.callAllfn();
+
+    await worker.terminate();
+  })();
+  res.json({ message: "Upload Succesful!!" });
+});
+
+
+module.exports = router;
